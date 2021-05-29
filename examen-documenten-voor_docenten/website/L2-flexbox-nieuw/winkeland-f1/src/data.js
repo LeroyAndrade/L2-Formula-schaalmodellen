7ecaaa911ea2cb359a20cab664d65bb76f57d6ea
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2021-05-30 00:02:23 +0200

    live url + Github opnieuw geupload
    Bestand blijft overschreven worden met minder data dan de lokaal daadwerkelijk heeft

--- examen-documenten-voor_docenten/website/L2-flexbox-nieuw/winkeland-f1/src/data.js
+++ examen-documenten-voor_docenten/website/L2-flexbox-nieuw/winkeland-f1/src/data.js
@@ -6,19 +6,19 @@ const data = {
    id: '1',
    name: 'Redbull 2016',
    price: 30,
-   image: './export-img/products/Redbull.jpg',
+   image: 'https://www.firesofdestruction.nl/bewijzenmap/l2/kid/p4/export-img/products/Ferrari-2012.png',
   },
   {
    id: '2',
    name: 'Ferrari-5',
    price: 45,
-   image: './export-img/products/Ferrari-553_squalo.png',
+   image: 'https://www.firesofdestruction.nl/bewijzenmap/l2/kid/p4/export-img/products/Ferrari-553_squalo.png',
   },
   {
    id: '3',
    name: 'Ferrari-2012',
    price: 19,
-   image: 'https://picsum.photos/id/21/3008/2008',
+   image: 'https://www.firesofdestruction.nl/bewijzenmap/l2/kid/p4/export-img/products/Ferrari-2012.png',
   },
  ],
 };
