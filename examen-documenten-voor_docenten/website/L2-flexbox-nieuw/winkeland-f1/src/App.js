22833f15ecd5644cf905b8253dbfef3f5de7e9ff
AndradeLeroy <47416005+AndradeLeroy@users.noreply.github.com>
2021-05-29 23:38:55 +0200

    Deze bestanden horen bij de oplossing
    Index.js roept nu de object bestanden anders op van data.js

--- examen-documenten-voor_docenten/website/L2-flexbox-nieuw/winkeland-f1/src/App.js
+++ examen-documenten-voor_docenten/website/L2-flexbox-nieuw/winkeland-f1/src/App.js
@@ -1,37 +1,31 @@
-//import logo from './logo.svg';
-import React from 'react';
-import './Header.css';
-// import './js/javaScript.js';
-
 import Header from './components/Header';
 import Main from './components/Main';
-import Winkelmand from './components/Winkelmand';
+import Basket from './components/Basket';
 import data from './data';
 import { useState } from 'react';
-//import Winkelmand from './components/winkelmand';
+
+
 
 function App() {
  //Roep de objecten producten op
  const { products } = data;
 
  //hook implementeer een state in een functioneel component
-                                  //useState winkelmand is default leeg
- const [cartItems] = useState([]);
- 
   return (
    <div className="App">
     {/*Menu REACT object*/}
     <Header></Header>
 
     {/*Winkelmand REACT object*/}
-    <Winkelmand>cartItems = {cartItems} </Winkelmand>
+    <Basket ></Basket>
 
     {/*Winkelmand Main object*/}
     <div className="row">
-     <Main> products = {products} </Main>
+    <Main products={products}></Main>
     </div>
    </div>
   );
 }
 
 export default App;
+
